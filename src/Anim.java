95250d63ba03947d3971e5bba7366e8913248a17
Mostafa-Abdel-Mowla <mostafaabdo7880@gmail.com>
2025-12-08 22:26:59 +0200

    اضفت تغييرات جديدة على ملفات اللعبة

--- src/Anim.java
+++ src/Anim.java
@@ -1,13 +1,8 @@
-package Example1;
 
 
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//package project;
+
+
 
 import com.sun.opengl.util.*;
 import java.awt.*;
--- src/mygame.java
+++ src/mygame.java
@@ -1,71 +1,71 @@
-
-
-
-// import com.sun.opengl.util.*;
-// import java.awt.*;
-// import java.awt.event.KeyEvent;
-// import javax.media.opengl.*;
-// import javax.swing.*;
-// public class mygame extends JFrame {
-
-
-//     public static void main(String[] args) {
-//         new mygame();
-//     }
-//     public mygame() {
-//         GLCanvas glcanvas;
-//         Animator animator;
-
-//         AnimListener listener = new AnimListener() {
-//             @Override
-//             public void init(GLAutoDrawable glAutoDrawable) {
-
-//             }
-
-//             @Override
-//             public void display(GLAutoDrawable glAutoDrawable) {
-
-//             }
-
-//             @Override
-//             public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
-
-//             }
-
-//             @Override
-//             public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
-
-//             }
-
-//             @Override
-//             public void keyTyped(KeyEvent e) {
-
-//             }
-
-//             @Override
-//             public void keyPressed(KeyEvent e) {
-
-//             }
-
-//             @Override
-//             public void keyReleased(KeyEvent e) {
-
-//             }
-//         };
-//         glcanvas = new GLCanvas();
-//         glcanvas.addGLEventListener(listener);
-//         glcanvas.addKeyListener(listener);
-//         getContentPane().add(glcanvas, BorderLayout.CENTER);
-//         animator = new FPSAnimator(15);
-//         animator.add(glcanvas);
-//         animator.start();
-
-//         setTitle("Anim Test");
-//         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//         setSize(700, 700);
-//         setLocationRelativeTo(null);
-//         setVisible(true);
-//         setFocusable(true);
-//         glcanvas.requestFocus();
-//     }
-// }
+//
+//
+//
+//import com.sun.opengl.util.*;
+//import java.awt.*;
+//import java.awt.event.KeyEvent;
+//import javax.media.opengl.*;
+//import javax.swing.*;
+//public class mygame extends JFrame {
+//
+//
+//    public static void main(String[] args) {
+//        new mygame();
+//    }
+//    public mygame() {
+//        GLCanvas glcanvas;
+//        Animator animator;
+//
+//        AnimListener listener = new AnimListener() {
+//            @Override
+//            public void init(GLAutoDrawable glAutoDrawable) {
+//
+//            }
+//
+//            @Override
+//            public void display(GLAutoDrawable glAutoDrawable) {
+//
+//            }
+//
+//            @Override
+//            public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+//
+//            }
+//
+//            @Override
+//            public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
+//
+//            }
+//
+//            @Override
+//            public void keyTyped(KeyEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void keyPressed(KeyEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void keyReleased(KeyEvent e) {
+//
+//            }
+//        };
+//        glcanvas = new GLCanvas();
+//        glcanvas.addGLEventListener(listener);
+//        glcanvas.addKeyListener(listener);
+//        getContentPane().add(glcanvas, BorderLayout.CENTER);
+//        animator = new FPSAnimator(15);
+//        animator.add(glcanvas);
+//        animator.start();
+//
+//        setTitle("Anim Test");
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setSize(700, 700);
+//        setLocationRelativeTo(null);
+//        setVisible(true);
+//        setFocusable(true);
+//        glcanvas.requestFocus();
+//    }
+//}
