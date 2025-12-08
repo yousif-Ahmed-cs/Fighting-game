b14991fff50a90d72fa8d9d45591c89c96ccd8c7
yousif-Ahmed-cs <josefahmed357@gmail.com>
2025-12-08 18:36:01 +0200

    Added new classes and initial game code

---
+++ src/AnimListener.java
@@ -0,0 +1,9 @@
+
+import java.awt.event.KeyListener;
+import javax.media.opengl.GLEventListener;
+
+public abstract class AnimListener implements GLEventListener, KeyListener {
+
+    protected String assetsFolderName = "Assets";
+
+}
