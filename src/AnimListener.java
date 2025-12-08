26d567be28b74e235236423eb5670d197e0131f6
Mostafa-Abdel-Mowla <mostafaabdo7880@gmail.com>
2025-12-08 20:56:44 +0200

    mostafa

--- src/AnimListener.java
+++ src/AnimListener.java
@@ -1,8 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import javax.media.opengl.GLEventListener;
 
-public abstract class AnimListener implements GLEventListener, KeyListener {
+/**
+ *
+ * @author Tyba
+ */
+
+public abstract class AnimListener implements GLEventListener, KeyListener  {
 
     protected String assetsFolderName = "Assets";
 
