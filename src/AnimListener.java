b2e48ef92bcd954d26a9e3cd2478ac83e598869a
Mostafa-Abdel-Mowla <mostafaabdo7880@gmail.com>
2025-12-08 21:17:31 +0200

    Clean up AnimListener.java by removing package and comments
    Removed package declaration and comments from AnimListener.java

--- src/AnimListener.java
+++ src/AnimListener.java
@@ -1,8 +1,4 @@
-package Example1;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
