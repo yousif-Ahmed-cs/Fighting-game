479911bd76e1c2b6842c8c19d3cb20341ac70245
Mostafa-Abdel-Mowla <mostafaabdo7880@gmail.com>
2025-12-08 21:17:14 +0200

    Implement MouseListener in AnimListener class
    Added MouseListener to AnimListener class.

--- src/AnimListener.java
+++ src/AnimListener.java
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-=======
-/*
+package Example1;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
->>>>>>> 55e60f3b62d20e1ca5137aba4e00225016af1f46
 import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -17,7 +14,7 @@ import javax.media.opengl.GLEventListener;
  * @author Tyba
  */
 
-public abstract class AnimListener implements GLEventListener, KeyListener  {
+public abstract class AnimListener implements GLEventListener, KeyListener , MouseListener {
 
     protected String assetsFolderName = "Assets";
 
